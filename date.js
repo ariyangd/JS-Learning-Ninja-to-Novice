e97329e6d0579738f1808e4de12385a33de2d4de
ariyangd <57576103+ariyangd@users.noreply.github.com>
2021-06-06 22:22:57 +0800

    Done till date

---
+++ date.js
@@ -0,0 +1,42 @@
+// const now = new Date();
+// console.log(now.getHours());
+
+// const before = new Date('February 1 2019 7:30:59');
+// const now = new Date();
+
+// console.log(now.getTime(), before.getTime());
+
+// const diff = now.getTime()- before.getTime();
+
+// const Hours = Math.round(diff/1000/60/60);
+// console.log(Hours);
+
+////digital clock
+// const clock = document.querySelector('.clock');
+
+// const tick = () =>{
+//     const now = new Date();
+//     const h = now.getHours();
+//     const m = now.getMinutes();
+//     const s = now.getSeconds();
+//     console.log(h,m,s);
+
+//     const html = `
+//     <span>${h}</span>
+//     <span>${m}</span>
+//     <span>${s}</span>
+//     `;
+
+//     clock.innerHTML = html;
+    
+// }
+
+// setInterval(tick, 1000);
+
+////date library
+
+
+
+
+
+
